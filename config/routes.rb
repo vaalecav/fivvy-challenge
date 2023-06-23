c28038282b9dac4190fac9e8a5be132520963ddd
vaalecav <valit28@gmail.com>
2023-06-22 21:32:44 -0300

    Add base application

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# == Route Map
+#
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
