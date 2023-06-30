09a990c51fa278317243cf6208b9bb83fdd21c87
vaalecav <valit28@gmail.com>
2023-06-29 21:14:58 -0300

    Add acceptance model specs

--- spec/models/acceptance_spec.rb
+++ spec/models/acceptance_spec.rb
@@ -3,5 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Acceptance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:disclaimer) }
+  it { is_expected.to belong_to(:user) }
 end
